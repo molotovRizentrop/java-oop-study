1e713141f552fe6c1d134aac421be1c01c50212c
kashtan <bassanddub.co@gmail.com>
2023-09-07 18:53:47 +0300

    Added exeption example

---
+++ src/section081_exceptions/CheckedVsUncheckedExceptions.java
@@ -0,0 +1,53 @@
+/*
+ *
+ *  Copyright (c) 2023. @kashtan @email:bassanduub.co@gmail.com
+ *
+ *  The Java Study Project licenses this file to you under the Apache License,
+ *  version 2.0 (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package section081_exceptions;
+
+import java.util.Scanner;
+
+/**
+ * @author:kashtan
+ * @email:bassanddub.co@gmail.com
+ **/
+public class CheckedVsUncheckedExceptions {
+    public static void main(final String[] args) {
+        runTest();
+    }
+
+    private static void runTest() {
+        runUncheckedExceptionExample();
+        try {
+            runCheckedExceptionExample();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void runUncheckedExceptionExample() {
+        final int value = new Scanner(System.in).nextInt();
+        if (value < 0) {
+            throw new IllegalArgumentException("Number must be positive!");
+        }
+    }
+
+    private static void runCheckedExceptionExample() throws Exception {
+        final int value = new Scanner(System.in).nextInt();
+        if (value < 0) {
+            throw new Exception("Number must be positive!");
+        }
+    }
+}

6605b941999e7f966bb2841d216ee158abf36a8f
kashtan <bassanddub.co@gmail.com>
2023-09-09 17:34:58 +0300

    Update ExceptionConversion.java
    Added reading value from SQL database

--- src/section091_exeption_usage/ExceptionConversion.java
+++ src/section091_exeption_usage/ExceptionConversion.java
@@ -20,32 +20,81 @@ package section091_exeption_usage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static java.sql.DriverManager.getConnection;
 
 /**
  * @author:kashtan
  * @email:bassanddub.co@gmail.com
  **/
 public class ExceptionConversion {
+
     public static void main(final String[] args) {
-        final ConfigProvider provider = new FileConfigProvider();
-        int configValue;
-        try {
-            configValue = provider.getConfigValue();
-        } catch (IOException exception) {
-            System.err.println("Can't read config value from privider " + provider.getClass() + ": " + exception);
-            configValue = 666;
+        final ConfigProvider[] providers = {
+                new FileConfigProvider(),
+                new RelationDatabaseConfigProvider()
+        };
+        int configValue = 666;
+        for (final ConfigProvider provider : providers) {
+            try {
+                configValue = provider.getConfigValue();
+                break;
+            } catch (final CantReadConfigValueException exception) {
+                System.err.println("Can't read config value from provider: " + provider.getClass() + ": " + exception);
+            }
         }
         System.out.println(configValue);
     }
 
     public interface ConfigProvider {
-        int getConfigValue() throws IOException;
+
+        int getConfigValue();
     }
 
-    private static class FileConfigProvider implements ConfigProvider {
+    public static final class CantReadConfigValueException extends RuntimeException {
+
+        public CantReadConfigValueException(final String message) {
+            super(message);
+        }
+
+        public CantReadConfigValueException() {
+            super();
+        }
+    }
+
+    public static class FileConfigProvider implements ConfigProvider {
+
         @Override
-        public int getConfigValue() throws IOException {
-            return Integer.parseInt(Files.readString(Paths.get(System.getProperty("user.home") + "/config-file.txt")));
+        public int getConfigValue() {
+            try {
+                return Integer.parseInt(Files.readString(Paths.get(System.getProperty("user.home") + "/config-file.txt")));
+            } catch (final IOException | NumberFormatException exception) {
+                throw new CantReadConfigValueException();
+            }
+        }
+    }
+
+    public static class RelationDatabaseConfigProvider implements ConfigProvider {
+
+        @Override
+        public int getConfigValue() {
+            try (Connection connection = getConnection("jdbc:postgresql://localhost:5432/db", "user", "password")) {
+                try (Statement statement = connection.createStatement()) {
+                    try (ResultSet resultSet = statement.executeQuery("SELECT config_value FROM config")) {
+                        if (resultSet.next()) {
+                            return resultSet.getInt("config_value");
+                        } else {
+                            throw new CantReadConfigValueException("Table 'config' does not contain value for the required 'config_value' column!");
+                        }
+                    }
+                }
+            } catch (final SQLException exception) {
+                throw new CantReadConfigValueException();
+            }
         }
     }
 }
